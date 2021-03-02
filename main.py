575cbb53d1c604fc24d415192a9f34958fb571e5
Ziro <petrop@purdue.edu>
2021-03-02 14:31:21 -0500

    runs  for part 1 (minus writing)

--- main.py
+++ main.py
@@ -7,6 +7,10 @@
 from BitVector import *
 from Crypto.Cipher import AES
 
+
+def byte_xor(ba1, ba2):
+    return bytes([_a ^ _b for _a, _b in zip(ba1, ba2)]) #code from https://nitratine.net/blog/post/xor-python-byte-strings/
+
 #Arguments:# v0: 128-bit BitVector object containing the seed value
 # dt: 128-bit BitVector object symbolizing the date and time
 # key_file: String of file name containing the encryption key (in ASCII) for AES
@@ -26,33 +30,40 @@ def x931(v0, dt, totalNum, key_file):
     key1 = key[0:16]
     key2 = key[16:32]
     #print(len(key1),len(key2))
+    bkey = bytes(key1, 'utf-8')
     bkey1  = bytes(key1, 'utf-8') #bytekey 1
     bkey2  = bytes(key2, 'utf-8') #bytekey 2
+    Randarr = []
 
     #Convert BV to byte vectors
     tv0 = v0.get_text_from_bitvector() #textv0
     print(tv0)
     bv0 = bytes(tv0, 'utf-8') #byte v0
     ddt = dt.int_val()
+    ddt = 99
     bdt = ddt.to_bytes(2,'big')
+    print("D",len(bdt))
+    print(ddt)
+    print(int.from_bytes(bdt,'big'))
+    print(bdt)
 
-    #begin EDE
-    kak = AES.new(bkey1,AES.MODE_EAX)
-    ede1 = kak.encrypt(bdt)
-    ede3 = ede1 ^ bv0
-    ede3 = kak.encrypt(ede3)
-    ede2 = kak.encrypt(ede3 ^ ede1)
-    v0 = ede2
 
-    rj = ede3
+    #PRNG Initializations
+    kak = AES.new(bkey,AES.MODE_EAX)
 
-    print("Aaa")
-    #EDE - 2x DES- Encrypt, decrypt, encryptds
-    #but where are the 2 keys it needs???
+    #PRNG loop
+    for i in range(totalNum):
+        ede1 = kak.encrypt(bdt)
+        ede3 = kak.encrypt(byte_xor(ede1,bv0))
+        Randarr.append(ede3)
+        ede2 = kak.encrypt(byte_xor(ede3,ede1))
+        bv0 = ede2
+        print(":)")
+    return Randarr
 
 if __name__ == '__main__':
     v0 = BitVector(textstring='computersecurity')  # v0 will be 128 bits
     # As mentioned before, for testing purposes dt is set to a predetermined value
     dt = BitVector(intVal=99, size=128)
     listX931 = x931(v0,dt,3,'keyX931.txt')
-    print("po")
+    write
